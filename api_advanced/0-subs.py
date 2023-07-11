e73930b522a494e6a53b950bd1f0aadd5d6d64b5
Samuel <s.olubode@alustudent.com>
2023-07-11 09:17:41 +0000

    task 0

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+""" Reddit API """
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """ num of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+
+    response = requests.get(url, \
+    headers={'User-Agent': 'Mozilla/5.0'})
+
+    if response.status_code != 200:
+        return 0
+    else:
+        json_response = response.json()
+        return json_response.get('data').get('subscribers')
