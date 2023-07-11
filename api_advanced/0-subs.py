0a6397024b8592880bf032e78e9fb72847313320
Samuel <s.olubode@alustudent.com>
2023-07-11 09:24:42 +0000

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,8 +9,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
-    response = requests.get(url, 
-        headers={'User-Agent': 'Mozilla/5.0'})
+    response = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
 
     if response.status_code != 200:
         return 0
